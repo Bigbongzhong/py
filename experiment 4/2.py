85fa90719a4268907adc2d17065292c40a9ce7d1
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2025-01-29 15:03:48 +0530

    python lab changes

--- experiment 4/2.py
+++ experiment 4/2.py
@@ -1,14 +1,14 @@
-num = int(input("Enter a number: "))
-t = num
-num1 = str(num)
-power = len(num1)
-sum_of_powers = 0
-while t > 0:
-    digit = t% 10
-    sum_of_powers += digit ** power
-    t //= 10
-
-if sum_of_powers == num:
-    print(num, " is an Armstrong number.")
-else:
-    print(num,"is not an Armstrong number.")
+l1=[]
+for num in range(1,1001):
+    t = num
+    num1 = str(num)
+    power = len(num1)
+    sum_of_powers = 0
+    while t > 0:
+        digit = t% 10
+        sum_of_powers += digit ** power
+        t //= 10
+    if sum_of_powers == num:
+        # print(num , end=" ")
+        l1.append(num)
+print(l1)
---
+++ experiment 4/slic.py
@@ -0,0 +1,12 @@
+l2=[3,4,5]
+li=[1,"sajid", "true", 7.5, "sajal"]
+li.append("suryansh")
+li.insert(2,40)
+li.extend(l2)
+print(li)
+l3=[3,56,8,8,6,4,32]
+l3.remove(8)
+l3.pop(1)
+del l3[0]
+l3.update(1, 10)
+print(l3)
