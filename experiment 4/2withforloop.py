669b55019978a5f33d05a1221b62edaebddf60bd
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2025-01-30 10:08:45 +0530

    ayee

--- experiment 4/2withforloop.py
+++ experiment 4/2withforloop.py
@@ -1,11 +1,14 @@
-n = 153
-num_digits = len(str(n))
-sum = 0
-
-for digit in str(n):
-    sum += int(digit) ** num_digits
-
-if sum == n:
-    print(n, "is an Armstrong number")
-else:
-    print(n, "is not an Armstrong number")
+li=[]
+for j in range(20,1000):
+    num_digits = len(str(j))
+    sum = 0
+    n=j
+    for digit in str(j):
+        sum += int(digit) ** num_digits
+    # print(sum)
+    if sum == n:
+        # print(n, "is an Armstrong number")
+        li.append(j)
+    # else:
+    #     print(n, "is not an Armstrong number")
+print(li)
---
+++ experiment 4/class.py
@@ -0,0 +1,15 @@
+li=[]
+l1=[]
+# for i in range(50):
+#     li.append(i**2)
+# print(li)
+# l1.extend([li])
+# print(l1)
+# l1=li[2:6]
+# print(l1)
+li=[1,2,3,4,5]
+
+for i,index in enumerate(li):
+    print(index,"is at index no:",i)
+for i in range(len(li)):
+    print(i)
