57338567fc1db3af59509b23be58c6217eb76085
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2025-02-05 10:50:42 +0530

    set

---
+++ class work/update.py
@@ -0,0 +1,9 @@
+# a={1,2,3,4,5,}
+# b={1,2,6,7,8,9}
+# c=a.union_update(b)
+# print(c)
+
+#convert a list of odd numbers into a set using list comprehension
+
+set=set ([i for i in range(1000) if i%2!=0])
+print(set)
